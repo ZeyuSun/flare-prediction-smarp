b367b88864c24325d80e607fc7134f4b134133a5
Zeyu Sun <zeyusun@umich.edu>
2021-05-05 22:32:32 -0400

    added GOES downloading script

--- download.py
+++ download.py
@@ -1,9 +1,11 @@
 import os
 from multiprocessing import Pool
 from datetime import datetime, timedelta
-import drms
 import pandas as pd
 from tqdm import tqdm, trange
+import drms
+from sunpy.time import TimeRange
+from sunpy.instr.goes import get_goes_event_list
 
 
 ######### Change these ##########
@@ -18,8 +20,10 @@ SHARP_VEC_HEADER_DIR = os.path.join(DATA_DIR, 'SHARP/header_vec')
 SHARP_IMAGE_DIR = os.path.join(DATA_DIR, 'SHARP/image')
 SMARP_HEADER_DIR = os.path.join(DATA_DIR, 'SMARP/header')
 SMARP_IMAGE_DIR = os.path.join(DATA_DIR, 'SMARP/image')
+GOES_DIR = os.path.join(DATA_DIR, 'GOES')
 for folder in [SHARP_LOS_HEADER_DIR, SHARP_VEC_HEADER_DIR, SHARP_IMAGE_DIR,
-               SMARP_HEADER_DIR, SMARP_IMAGE_DIR]:
+               SMARP_HEADER_DIR, SMARP_IMAGE_DIR,
+               GOES_DIR]:
     if not os.path.exists(folder):
         os.makedirs(folder)
 
@@ -132,13 +136,42 @@ def download_smarp_images(tarpnum):
     return 0
 
 
-def batch_run(func, total_num, batch_size, num_workers=8):
+def download_goes_per_year(year):
+    t_start = datetime(year=year, month=1, day=1)
+    t_end = datetime(year=year+1, month=1, day=1)
+    timerange = TimeRange(t_start, t_end)
+    event_list = get_goes_event_list(timerange)
+    if len(event_list) == 0:
+        return None
+
+    event_df = pd.DataFrame(event_list)
+    event_df = event_df[event_df['noaa_active_region'] != 0]
+    if len(event_df) == 0:
+        return None
+
+    return event_df
+
+
+def download_goes():
+    # Up until 2021-05-21, the last record retrieved by sunpy is a B3.2 flare
+    # at 2020-12-23T05:53:00 in AR 12795.
+    year_range = range(1996, 2022)
+    with Pool(24) as pool:
+        df_list = pool.map(download_goes_per_year, year_range)
+
+    print("These years don't have GOES events assigned with NOAA AR: {}".format(
+          [year_range[i] for i, df in enumerate(df_list) if df is None]))
+    event_df = pd.concat(df_list).reset_index(drop=True)
+    event_df.to_csv(os.path.join(GOES_DIR, 'goes.csv'), index=None)
+
+
+def batch_run(func, max_num, batch_size, num_workers=8):
     """Parallelize func by batch with progress bar"""
-    pbar = trange(0, total_num, batch_size)
+    pbar = trange(0, max_num+1, batch_size)
     results = []
     for start in pbar:
-        stop = start + batch_size
-        pbar.set_description('Batch [%d, %d) / %d' % (start, stop, total_num))
+        stop = min(start + batch_size, max_num+1)
+        pbar.set_description('Batch [%d, %d) / %d' % (start, stop, max_num+1))
         batch = range(start, stop)
         with Pool(num_workers) as p:
             results.extend(p.map(func, batch))
@@ -153,13 +186,16 @@ def analyze(results, tag):
 
 
 if __name__ == '__main__':
-    results = batch_run(download_sharp_headers, 8000, 100)
+    download_goes()
+
+    # Last Record = su_mbobra.sharp_loskeystest_720s[7545][2021.02.17_19:36:00_TAI]
+    results = batch_run(download_sharp_headers, 7545, 100)
     analyze(results, 'sharp_headers')
 
     results = batch_run(download_smarp_headers, 14000, 1000)
     analyze(results, 'smarp_headers')
 
-    results = batch_run(download_sharp_images, 8000, 100)
+    results = batch_run(download_sharp_images, 7545, 100)
     analyze(results, 'sharp_images')
 
     results = batch_run(download_smarp_images, 14000, 1000)
