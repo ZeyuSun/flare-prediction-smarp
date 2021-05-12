630449d54d2f7440f601d39f9f8dc8565bea6e83
Zeyu Sun <zeyusun@umich.edu>
2021-05-12 09:14:32 -0400

    added draw_pairplot

--- utils.py
+++ utils.py
@@ -34,3 +34,25 @@ def get_log_intensity(class_str):
     a = class_map[class_str[0]]
     b = np.log10(float(class_str[1:]))
     return a + b
+
+
+def draw_pairplot(Xs, labels, keys):
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    Xs = [X[np.random.choice(len(X), size=1000, replace=False)] if len(X) > 1000 else X
+          for X in Xs]
+
+    df = pd.DataFrame(np.concatenate(Xs), columns=keys)
+    labels = np.concatenate([[label] * len(X) for X, label in zip(Xs, labels)])
+    df = df.assign(label=labels)
+    sns.pairplot(df, hue='label', corner=True,
+                 kind='kde', # both diag and off-diag
+                 #diag_kind = 'kde',
+                 diag_kws = {'common_norm': False}, # kdeplot(univariate)
+                 plot_kws = {'common_norm': False, 'levels': 5}, # kdeplot(bivariate)
+    )
+    #plt.savefig('outputs/pairplot.png')
+    plt.show()
