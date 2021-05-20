2c1b08c86cb2bbb613d07a49d1c20bdd2c0d259b
Zeyu Sun <zeyusun@umich.edu>
2021-05-19 22:35:44 -0400

    automate multiple dataset preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import logging
 from multiprocessing import Pool
 from collections import defaultdict
@@ -7,30 +8,10 @@ from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
 import drms
-from tqdm import tqdm, trange
+from tqdm import tqdm
 
 from arnet.utils import read_header, query
 from utils import get_flare_index
-
-
-######### Change these ##########
-RAW_DATA_DIR = '/data2'
-PROCESSED_DATA_DIR = 'datasets/M1.0_24hr_balanced'
-#################################
-
-
-GOES_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.000'
-KEYWORDS = ['AREA', 'USFLUX', 'MEANGBZ', 'R_VALUE']
-goes = pd.read_csv(os.path.join(RAW_DATA_DIR, 'GOES/goes.csv'))
-goes['goes_class'] = goes['goes_class'].fillna('')
-if not os.path.exists(PROCESSED_DATA_DIR):
-    os.makedirs(PROCESSED_DATA_DIR)
-logging.basicConfig(filename=os.path.join(PROCESSED_DATA_DIR, 'log_preprocess.txt'),
-                    filemode='a',
-                    format='[%(asctime)s] %(name)s %(levelname)s: %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    level=logging.INFO)
-logger = logging.getLogger()
 
 
 def get_prefix(dataset):
@@ -46,18 +27,18 @@ def get_prefix(dataset):
 def get_image_filepath(dataset, arpnum, t_rec):
     t_rec = t_rec.strftime('%Y%m%d_%H%M%S_TAI')
     if dataset == 'sharp':
-        return os.path.join(RAW_DATA_DIR, f'SHARP/image/{arpnum:06d}/hmi.sharp_cea_720s.{arpnum}.{t_rec}.magnetogram.fits')
+        return os.path.join(args.raw_data_dir, f'SHARP/image/{arpnum:06d}/hmi.sharp_cea_720s.{arpnum}.{t_rec}.magnetogram.fits')
     elif dataset == 'smarp':
-        return os.path.join(RAW_DATA_DIR, f'SMARP/image/{arpnum:06d}/su_mbobra.smarp_cea_96m.{arpnum}.{t_rec}.magnetogram.fits')
+        return os.path.join(args.raw_data_dir, f'SMARP/image/{arpnum:06d}/su_mbobra.smarp_cea_96m.{arpnum}.{t_rec}.magnetogram.fits')
     else:
         raise
 
 
 def split(dataset, split_num, seed=None):
     if dataset == 'sharp':
-        header_dir = os.path.join(RAW_DATA_DIR, 'SHARP/header_vec')
+        header_dir = os.path.join(args.raw_data_dir, 'SHARP/header_vec')
     elif dataset == 'smarp':
-        header_dir = os.path.join(RAW_DATA_DIR, 'SMARP/header')
+        header_dir = os.path.join(args.raw_data_dir, 'SMARP/header')
     else:
         raise
     header_files = sorted(os.listdir(header_dir))
@@ -70,13 +51,15 @@ def split(dataset, split_num, seed=None):
     return splits
 
 
-def select(dataset, arpnums):
+def select(dataset, arpnums, val_time, criterion):
     # Non-parallel
     #samples = map(partial(select_per_arp, dataset), arpnums)
 
     # Parallel
     with Pool(24) as pool:
-        samples = pool.map(partial(select_per_arp, dataset), arpnums)
+        closure = partial(select_per_arp, dataset,
+                          val_time=val_time, criterion=criterion)
+        samples = pool.map(closure, arpnums)
 
     samples = [s for s in samples if s is not None]
     samples = [i for s in samples for i in s]  # concatenate
@@ -85,7 +68,8 @@ def select(dataset, arpnums):
 
 
 #@profile
-def select_per_arp(dataset, arpnum):
+def select_per_arp(dataset, arpnum,
+                   val_time=None, criterion=None):
     """
     Args:
         dataset (str): 'smarp' or 'sharp'
@@ -99,7 +83,6 @@ def select_per_arp(dataset, arpnum):
         return None
 
     # Only keep observations near central meridian
-    LON_MIN, LON_MAX = -70, 70
     df = df[(df['LON_MIN'] >= LON_MIN) & (df['LON_MAX'] <= LON_MAX)]
     if len(df) == 0:
         return None
@@ -109,12 +92,10 @@ def select_per_arp(dataset, arpnum):
     noaa_ars = df['NOAA_ARS'].unique() # Series.unique returns numpy.ndarray
     assert len(noaa_ars) == 1
     noaa_ars = [int(ar) for ar in noaa_ars[0].split(',')]
-    goes_ar = goes[goes['noaa_active_region'].isin(noaa_ars)]
+    goes_ar = GOES[GOES['noaa_active_region'].isin(noaa_ars)]
 
     # For SHARP, only keep observations between 2010.10.29 and 2020.12.01
     df['T_REC'] = df['T_REC'].apply(drms.to_datetime)
-    T_REC_MIN = datetime(year=2010, month=10, day=29)
-    T_REC_MAX = datetime(year=2020, month=12, day=1)
     if dataset == 'sharp':
         df = df[(df['T_REC'] >= T_REC_MIN) &
                 (df['T_REC'] <= T_REC_MAX)]
@@ -129,8 +110,6 @@ def select_per_arp(dataset, arpnum):
         df.loc[idx, 'image_nan'] = np.any(np.isnan(image_data))
 
     # 2nd scan: generate sequences
-    OBS_TIME = timedelta(days=1)  # observation time
-    VAL_TIME = timedelta(days=1)  # prediction validity period
     samples = []
     counter = defaultdict(int)
     for idx in df.index:
@@ -154,21 +133,20 @@ def select_per_arp(dataset, arpnum):
             counter['nan_val_last'] += 1
             continue
 
-        t_after = t_end + VAL_TIME
+        t_after = t_end + val_time
         t_before = t_end - OBS_TIME
         flares_after = goes_ar.loc[(goes_ar['start_time'] >= t_end.strftime(GOES_TIME_FORMAT)) &
                                    (goes_ar['start_time'] <= t_after.strftime(GOES_TIME_FORMAT)),
                                    'goes_class'].tolist()
         flares_after = '|'.join(flares_after)
-        label = 'M' in flares_after or 'X' in flares_after
-
         flares_before = goes_ar.loc[(goes_ar['start_time'] >= t_before.strftime(GOES_TIME_FORMAT)) &
                                     (goes_ar['start_time'] <= t_end.strftime(GOES_TIME_FORMAT)),
                                     'goes_class'].tolist()
         flare_index = get_flare_index(flares_before)
         flares_before = '|'.join(flares_before)
-        if not label and (len(flares_before) > 0  or len(flares_after) > 0):
-            # Only select queit samples for the negative class
+
+        label = get_label(flares_before, flares_after, criterion)
+        if label is None:
             counter['pos'] += 1
             continue
 
@@ -195,37 +173,44 @@ def select_per_arp(dataset, arpnum):
         }
         sample.update({k: df.loc[mask, k].iloc[-1] for k in KEYWORDS})
         samples.append(sample)
-    logger.info('{} {}: {}/{} sequences extracted. {}'.format(get_prefix(dataset), arpnum, len(samples), len(df), dict(counter)))
+    logger.info('{} {}: {}/{} sequences extracted. {}'.format(
+        get_prefix(dataset), arpnum, len(samples), len(df), dict(counter)))
     return samples
 
 
-def main(dataset, split_num, output_dir):
+def main(dataset, split_num, output_dir, val_time, criterion):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    splits = split(dataset, split_num, seed=0)
+    splits = split(dataset, split_num, seed=args.seed)
     df_list = []
     for key, arpnums in tqdm(enumerate(splits)):
         logger.info(f'{dataset} split {key} / {len(splits)-1}: {arpnums}')
-        df = select(dataset, arpnums)
+        df = select(dataset, arpnums, val_time, criterion)
         df_list.append(df)
 
     train_df = pd.concat(df_list[:split_num-1]).reset_index(drop=True) #TODO: sort by dataset/arpnum
     train_df.to_csv(os.path.join(output_dir, 'train.csv'), index=False)
 
-    train_df = rus(train_df, seed=0)
+    train_df = rus(train_df)
     train_df.to_csv(os.path.join(output_dir, 'train_balanced.csv'), index=False)
 
     test_df = df_list[-1]
     test_df.to_csv(os.path.join(output_dir, 'test.csv'), index=False)
 
-    test_df = rus(test_df, seed=1)
+    test_df = rus(test_df)
     test_df.to_csv(os.path.join(output_dir, 'test_balanced.csv'), index=False)
 
 
-def rus(df, seed=None):
-    """Random Undersampling"""
-    np.random.seed(seed)
+def rus(df, seed=False):
+    """Random Undersampling
+
+    Args:
+        seed: default is False, do not reset seed. Pass int/None to reset a
+            seed particularly/randomly.
+    """
+    if seed is None or isinstance(seed, int):
+        np.random.seed(seed)
     pos_mask = df['label'].to_numpy()
     neg_mask = (~df['label']).to_numpy()
     drop_idx = np.random.choice(np.nonzero(neg_mask)[0],
@@ -236,6 +221,25 @@ def rus(df, seed=None):
     return df
 
 
+def get_label(flares_before, flares_after, criterion='MX_Q'):
+    """
+    Args:
+        flares_before: Flares in the observation time
+        flares_after: Flares in the prediction window
+        criterion: Classification criterion
+    """
+    pos, neg = criterion.split('_')
+    assert pos in ['X', 'MX']
+    label = any([p in flares_after for p in pos])
+    if not label:
+        if neg == 'Q':
+            if len(flares_before) > 0 or len(flares_after) > 0:
+                return None
+        else:
+            raise
+    return label
+
+
 def test_seed():
     np.random.seed(0)
     a = np.random.randint(0, 65536, 10)
@@ -243,7 +247,42 @@ def test_seed():
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--raw_data_dir', default='/data2')
+    parser.add_argument('--processed_data_dir', default='datasets')
+    parser.add_argument('--seed', default=0)
+    args = parser.parse_args()
+
+    # global variables
+    LON_MIN, LON_MAX = -70, 70
+    T_REC_MIN = datetime(year=2010, month=10, day=29)
+    T_REC_MAX = datetime(year=2020, month=12, day=1)
+    OBS_TIME = timedelta(days=1)  # observation time
+    GOES_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.000'
+    KEYWORDS = ['AREA', 'USFLUX', 'MEANGBZ', 'R_VALUE']
+    GOES = pd.read_csv(os.path.join(args.raw_data_dir, 'GOES/goes.csv'))
+    GOES['goes_class'] = GOES['goes_class'].fillna('')
+    if not os.path.exists(args.processed_data_dir):
+        os.makedirs(args.processed_data_dir)
+    logging.basicConfig(filename=os.path.join(args.processed_data_dir, 'log_preprocess.txt'),
+                        filemode='a',
+                        format='[%(asctime)s] %(name)s %(levelname)s: %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO)
+    logger = logging.getLogger()
+
+    # begin preprocessing
     test_seed()
-    for dataset in ['smarp', 'sharp']:
-        output_dir = os.path.join(PROCESSED_DATA_DIR, dataset)
-        main(dataset, split_num=5, output_dir=output_dir)
+    for criterion in ['MX_Q']:
+        for val_hours in [6, 12, 24]:
+            for dataset in ['smarp', 'sharp']:
+                output_dir = os.path.join(args.processed_data_dir,
+                                          f'{criterion}_{val_hours}hr',
+                                          dataset)
+                logger.info(output_dir)
+                print(output_dir)
+                main(dataset,
+                     split_num=5,
+                     output_dir=output_dir,
+                     val_time=timedelta(hours=val_hours),
+                     criterion=criterion)
