cbd6b16233732db1da8ae60b781c747d9b58a927
Zeyu Sun <zeyusun@umich.edu>
2021-07-15 10:17:27 -0400

    leaderboard4: imbalanced dataset

--- run_arnet.py
+++ run_arnet.py
@@ -143,8 +143,8 @@ def sweep():
     parser.add_argument('-d', '--data_root', default='datasets')
     parser.add_argument('-c', '--config_root', default='arnet/configs')
     parser.add_argument('-s', '--smoke', action='store_true')
-    parser.add_argument('-e', '--experiment_name', default='leaderboard3')
-    parser.add_argument('-r', '--run_name', default='CNN_last_model_path')
+    parser.add_argument('-e', '--experiment_name', default='leaderboard4')
+    parser.add_argument('-r', '--run_name', default='CNN_LSTM_imbalance')
     parser.add_argument('opts', default=None, nargs=argparse.REMAINDER)
     args = parser.parse_args()
     if args.smoke:
@@ -161,11 +161,11 @@ def sweep():
     databases = [p for p in Path(args.data_root).iterdir() if p.is_dir()]
     databases = [Path(args.data_root) / d for d in ['M_Q_24hr']]
     #configs = [c for c in Path(args.config_root).iterdir()]
-    configs = [Path('arnet/configs') / f'{c}.yaml' for c in ['CNN']]
+    configs = [Path('arnet/configs') / f'{c}.yaml' for c in ['LSTM', 'CNN']]
     mlflow.set_experiment(args.experiment_name)
     with mlflow.start_run(run_name=args.run_name):
         for database in databases:
-            for balanced in [True]:
+            for balanced in [False]:
                 for dataset in ['sharp', 'fused_sharp', 'smarp', 'fused_smarp']:
                     for config in configs:
                         for seed in range(5):
