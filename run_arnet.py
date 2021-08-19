ed2b596b684b6bd9d96d47271a0e01b39da971d2
Zeyu Sun <zeyusun@umich.edu>
2021-08-19 01:05:49 -0400

    Repeat CNN experiment

--- run_arnet.py
+++ run_arnet.py
@@ -150,7 +150,7 @@ def sweep():
     parser.add_argument('-c', '--config_root', default='arnet/configs')
     parser.add_argument('-s', '--smoke', action='store_true')
     parser.add_argument('-e', '--experiment_name', default='leaderboard3')
-    parser.add_argument('-r', '--run_name', default='base')
+    parser.add_argument('-r', '--run_name', default='base_rep_CNN')
     parser.add_argument('opts', default=None, nargs=argparse.REMAINDER)
     args = parser.parse_args()
     if args.smoke:
@@ -167,7 +167,7 @@ def sweep():
     databases = [p for p in Path(args.data_root).iterdir() if p.is_dir()]
     databases = [Path(args.data_root).absolute() / d for d in ['M_Q_24hr']]
     #configs = [c for c in Path(args.config_root).iterdir()]
-    configs = [Path('arnet/configs').absolute() / f'{c}.yaml' for c in ['LSTM', 'CNN']]
+    configs = [Path('arnet/configs').absolute() / f'{c}.yaml' for c in ['CNN']]
     mlflow.set_experiment(args.experiment_name)
     with mlflow.start_run(run_name=args.run_name):
         for database in databases:
