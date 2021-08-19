dba751d2ee42a21ced5ed7fd4d920bcd58a8b2f1
Zeyu Sun <zeyusun@umich.edu>
2021-08-19 03:14:57 -0400

    Conv kernel visualization: fix color scale

--- arnet/utils/visualization.py
+++ arnet/utils/visualization.py
@@ -76,15 +76,29 @@ def fig2rgb(fig):
     return image
 
 def draw_conv2d_weight(weight):
+    import matplotlib as mpl
+
     # Conv3d weight has shape (64, 1, 1, 11, 11)
     filters = weight.detach().cpu().numpy()
     n = int(np.ceil(np.sqrt(len(filters))))
-    fig = plt.figure(figsize=(10,10))
+    vmin, vmax = filters.min(), filters.max()
+
+    widths = [3] * n + [0.4]
+    heights = [3] * n
+    fig = plt.figure(figsize=(11,10))
+    gs = mpl.gridspec.GridSpec(ncols=len(widths), nrows=len(heights), figure=fig,
+                               width_ratios=widths, height_ratios=heights)
+    gs.update(wspace=0.05, hspace=0.05)
     for i in range(n):
         for j in range(n):
-            plt.subplot(n, n, i*n+j+1)
-            plt.imshow(filters[i*n+j, 0, 0, :, :], cmap='gray')
-            plt.axis('off')
+            ax = fig.add_subplot(gs[i, j])
+            ax.imshow(filters[i*n+j, 0, 0, :, :],
+                      cmap='gray', vmin=vmin, vmax=vmax)
+            ax.axis('off')
+    ax = fig.add_subplot(gs[:,-1])
+    norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+    cb = mpl.colorbar.ColorbarBase(ax, cmap=plt.get_cmap('gray'), norm=norm)
+    #plt.tight_layout() # this figure includes Axes that are not compatible with tight_layout, so results might be incorrect.
     plt.close()
     return fig
 
