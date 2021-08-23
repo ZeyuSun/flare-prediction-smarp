604113c7e0dd7ccb2cd65df7113fccedd9290ebc
Zeyu Sun <zeyusun@umich.edu>
2021-08-23 05:20:42 -0400

    Change run_name to val_tss

--- run_arnet.py
+++ run_arnet.py
@@ -156,7 +156,7 @@ def sweep():
     parser.add_argument('-c', '--config_root', default='arnet/configs')
     parser.add_argument('-s', '--smoke', action='store_true')
     parser.add_argument('-e', '--experiment_name', default='leaderboard3')
-    parser.add_argument('-r', '--run_name', default='cnn_li2020')
+    parser.add_argument('-r', '--run_name', default='val_tss')
     parser.add_argument('opts', default=None, nargs=argparse.REMAINDER)
     args = parser.parse_args()
     if args.smoke:
