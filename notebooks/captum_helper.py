9aa1a9ff015a6a92a9840e2cac50bb70492884e6
Zeyu Sun <zeyusun@umich.edu>
2021-09-23 06:22:01 -0400

    Improve Captum interface

--- notebooks/captum_helper.py
+++ notebooks/captum_helper.py
@@ -3,37 +3,90 @@ from ipdb import set_trace as breakpoint
 import numpy as np
 import pandas as pd
 import xarray as xr
+import matplotlib.pyplot as plt
+from tqdm.notebook import tqdm
 import torch
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose
 from skimage.transform import resize
-from captum.attr import IntegratedGradients, Saliency, DeepLift, GuidedGradCam, NoiseTunnel, LayerGradCam, LayerLRP
+from scipy.ndimage import gaussian_filter
+from captum.attr import IntegratedGradients, Saliency, DeepLift, GuidedBackprop, GuidedGradCam, Deconvolution, NoiseTunnel, LRP, LayerGradCam, LayerLRP
+
+from arnet.utils import get_layer
+from arnet.dataset import ActiveRegionDataModule, ActiveRegionDataset
+from arnet.transforms import get_transform
+from cotrain_helper import get_learner_by_query
 
 
-def get_heatmap(algorithm, learner, input, target='negate'):
+def get_heatmap(algorithm, learner, input, target='negate', baselines='zero'):
     """
     TODO: heatmaps should only have 1 channels
     """
     model = learner.model
     model.eval()
     model.zero_grad()
-    if target == 'negate':
+    if target == 'negate': # negate should be taken care of inside function.
         logit = model(input)
         pred = logit[:, 1] > logit[:, 0]
         target = 1 - pred.to(int)
+    else: # target is a tensor
+        target = torch.tensor(target) # target may be an int, e.g., 1
+        target = target.to(input.device)
+
+    if baselines == 'zero':
+        baselines = input * 0
 
     if algorithm == 'Original':
         heatmap = input
-    elif algorithm == 'Saliency':
+    elif algorithm == 'Saliency':  # (Simonyan 2013)
         saliency = Saliency(model)
         heatmap = saliency.attribute(
             input,
             target=target,
         )
-    elif algorithm == 'IntegratedGradients':
+    elif algorithm == 'Deconvolution':  # (Zeiler 2013)
+        deconv = Deconvolution(model)
+        heatmap = deconv.attribute(
+            input,
+            target=target,
+        )
+    elif algorithm == 'GuidedBackprop':  # (Springenberg 2014)
+        guided_backprop = GuidedBackprop(model)
+        heatmap = guided_backprop.attribute(
+            input,
+            target=target,
+        )
+    elif algorithm[:12] == 'LayerGradCam':  # (Selvaraju 2016)
+        if algorithm == 'LayerGradCam':
+            algorithm += '-5' # last layer is 5
+        n_layer = int(algorithm.split('-')[1])
+        layer = get_layer(model, f'convs.conv{n_layer}')
+        layer_gradcam = LayerGradCam(model, layer)
+        heatmap = layer_gradcam.attribute(
+            input,
+            target=target,
+            #relu_attributions=True,
+        )
+        # up to 5D input is supported: (B, C, [D], [H], W)
+        # the `size` argument does not include B and C
+        heatmap = torch.nn.functional.interpolate(
+            heatmap,
+            size=input[0, 0].shape,
+            mode='trilinear',
+            align_corners=False,
+        )
+    elif algorithm == 'GuidedGradCam':  # (Selvaraju 2016)
+        guided_gradcam = GuidedGradCam(model, model.convs.conv4) #conv5)
+        heatmap = guided_gradcam.attribute(
+            input,
+            target=target,
+        )
+    elif algorithm == 'IntegratedGradients':  # (Sundararajan 2017)
         ig = IntegratedGradients(model)
         heatmap, delta = ig.attribute(
             input,
             target=target,
-            baselines=input * 0,
+            baselines=baselines,
             return_convergence_delta=True
         )
         print('Approximation delta: ', abs(delta))
@@ -43,37 +96,24 @@ def get_heatmap(algorithm, learner, input, target='negate'):
         heatmap = nt.attribute(
             input,
             target=target,
-            baselines=input * 0,
+            baselines=baselines,
             nt_type='smoothgrad_sq',
             nt_samples=100,
             stdevs=0.2
         )
-    elif algorithm == 'DeepLift':
+    elif algorithm == 'DeepLift':  # (Shrikumar 2017)
         deeplift = DeepLift(model)
         heatmap = deeplift.attribute(
             input,
             target=target,
-            baselines=input * 0,
+            baselines=baselines,
         )
-    elif algorithm == 'GuidedGradCam':
-        guided_gradcam = GuidedGradCam(model, model.convs.conv4) #conv5)
-        heatmap = guided_gradcam.attribute(
+    elif algorithm == 'LRP':
+        # Does not support Conv3d
+        lrp = LRP(model)
+        heatmap = lrp.attribute(
             input,
             target=target,
-        )
-    elif algorithm == 'LayerGradCam':
-        layer_gradcam = LayerGradCam(model, model.convs.conv4)
-        heatmap = layer_gradcam.attribute(
-            input,
-            target=target,
-        )
-        # up to 5D input is supported: (B, C, [D], [H], W)
-        # the `size` argument does not include B and C
-        heatmap = torch.nn.functional.interpolate(
-            heatmap,
-            size=input[0, 0].shape,
-            mode='trilinear',
-            align_corners=False,
         )
     elif algorithm == 'LayerLRP':
         # Does not support Conv3d
@@ -95,8 +135,125 @@ def get_heatmap(algorithm, learner, input, target='negate'):
     return heatmap
 
 
-def get_t_steps(t_now: str):
-    num_frames, num_frames_after = 16, 15
+def get_heatmaps_from_df(df, algorithms,
+                         num_frames=1,
+                         num_frames_after=0,
+                         target_type='pos',
+                         baseline_type='zero'):
+    """
+    Args:
+        df: has columns 'model_query'. Index 0, 1, ...
+        algorithms (List[str]): list of attribution algortihm names.
+        target_type (str):
+            'pos': positive class
+            'negate': negate the prediction
+            'label': same with the label
+            other: pass directly
+        baseline_type (str):
+            'zero': (default) zero baseline.
+            'first': first row with the same model_query. User has to make sure
+                all rows in model_query subdf belong to the same active region.
+            other: pass directly.
+
+    Returns:
+        heatmaps: list of attribution heatmaps in the order of df.
+            Each heatmap is of shape (C, T, H, W) = (1, 1, 128, 128).
+
+    Note:
+    * This function involves no concept of animation. df in, heatmaps out.
+    * I once implemented for each entry in df, expand 24 hr forward and backward
+    in time. It relies on the ActiveRegionDataset's functionality to retrieve
+    more data, as opposed to relying on df. Cons: unnecessarily convoluted (just
+    use df with consecutive entries); prone to repetitive computation due to
+    video overlapping for consecutive entries; baselines has more than 1 frame.
+    """
+    assert num_frames == 1 # See Note
+    assert num_frames_after == 0 # See Note
+    heatmaps = {a: [None] * len(df) for a in algorithms}
+    for model_query, subdf in df.groupby('model_query'):
+        # # hotfix begin
+        # fields = model_query.split('/')
+        # fields.insert(-1, '0')
+        # model_query = '/'.join(fields)
+        # # hotfix end
+
+        learner = get_learner_by_query(model_query).to('cuda')
+        cfg = learner.cfg
+        transform = Compose([get_transform(name, cfg)
+                             for name in cfg.DATA.TRANSFORMS] +
+                            [lambda video: video.to('cuda')])
+        dataset = ActiveRegionDataset(
+            subdf,
+            features=['MAGNETOGRAM'],
+            num_frames=num_frames,
+            num_frames_after=num_frames_after,
+            transform=transform
+        )
+        dataloader = DataLoader(
+            dataset,
+            batch_size=16 if 'IntegratedGradients' not in algorithms else 2, # at most 4 if IG in algorithms
+            #shuffle=False,
+            #num_workers=8,
+            #pin_memory=False,
+            #drop_last=False,
+        )
+
+        for algorithm in tqdm(algorithms):
+            hs = []
+            for videos, sizes, labels, meta in dataloader:
+                if target_type == 'pos':
+                    target = 1
+                elif target_type == 'negate':
+                    target = 'negate'
+                elif target_type == 'label':
+                    target = labels
+                else: # option to pass directly
+                    target = target_type
+                if baseline_type == 'first':
+                    first_video = dataset[0][0].unsqueeze(0) # first dimensions is to be broadcasted if batch_size > 1
+                    baselines = first_video
+                elif baseline_type == 'zero':
+                    baselines = 'zero' # 0 input
+                else:
+                    baselines = baseline_type
+                h = [get_heatmap(algorithm,
+                                 learner,
+                                 videos[:, :, [t]],
+                                 target=target,
+                                 baselines=baselines
+                                ).cpu().detach().numpy()
+                     for t in range(videos.shape[2])]
+                h = np.concatenate(h, axis=2)
+                # h.shape == videos.shape = [N, C=1, T, H, W]
+                hs.append(h)
+            hmaps = np.concatenate(hs) # along the batch dimension
+
+            for i, m in zip(subdf.index, hmaps):
+                heatmaps[algorithm][i] = m
+    return heatmaps
+
+
+def get_heatmaps(Z_list, algorithms, device):
+    dataset_pos = [torch.tensor(np.expand_dims(Z, axis=(0,1))).to(learner.device).to(torch.float32)
+                   for Z in Z_list_pos]
+
+    dataloader = DataLoader(
+        dataset_pos,
+        batch_size=4,
+        #num_workers=0,
+    )
+
+    heatmaps = {a: [None] * len(df_pos) for a in algorithms}
+    for algorithm in tqdm(algorithms):
+        hs = []
+        for videos in tqdm(dataloader):
+            h = get_heatmap(algorithm, learner, videos).cpu().detach().numpy()
+            hs.append(h)
+        heatmaps[algorithm] = np.concatenate(hs)
+    return heatmaps
+
+
+def get_t_steps(t_now: str, num_frames=16, num_frames_after=15):
     dt = timedelta(minutes=96)
     t_now = datetime.strptime(t_now, '%Y-%m-%d %H:%M:%S')
     t_start = t_now - dt * (num_frames - 1)
@@ -107,9 +264,13 @@ def get_t_steps(t_now: str):
     
 def plot_heatmaps_info(imgs, algorithms, info,
                        zmin, zmax, color_continuous_scale,
-                       animation_frame=None):
+                       animation_frame=None,
+                       num_frames=16, num_frames_after=15):
     if animation_frame is not None:
-        t_steps = get_t_steps(info['t_end'])
+        # although there are two other keys ('prob', 'label') used for info, we should probabily define t_steps outside the function. Because defining it inside requires two other arguments (num_frames and num_frames_after) that are not used elsewhere.
+        # From the point view of atomic function, t_steps should be taken care of outside of the function. If you there are too many things outside, you need another level of abstraction.
+        # We also want prob and label to change with animation.
+        t_steps = get_t_steps(info['t_end'], num_frames=num_frames, num_frames_after=num_frames_after)
         dims = ('Algorithm', 'Time', 'h', 'w')
         coords = {'Algorithm': algorithms, 'Time': t_steps}
     else:
@@ -120,8 +281,11 @@ def plot_heatmaps_info(imgs, algorithms, info,
                         animation_frame=animation_frame, # 1 or None
                         facet_col=0)
     
-    val_split = info['model_query'].split('/')[4]
-    prob = info['prob' + val_split]
+    # how prob or prob0,1... are defined in df_box needs to be consistent.
+    # we can implement a heuristic here
+    #val_split = info['model_query'].split('/')[-3]
+    #prob = info['prob' + val_split]
+    prob = info['prob']
     label = info['label']
     fig = add_label(fig, label, label_color="Red" if label else "Green")
     fig = add_pred(fig, prob, pred_color="Red" if prob > 0.5 else "Green")
@@ -135,6 +299,34 @@ def plot_heatmaps_info(imgs, algorithms, info,
     #    config={'modeBarButtonsToAdd':['drawopenpath', 'eraseshape']}
     #)
     return fig
+
+
+def plot_heatmaps_contour(images, attribution_maps):
+    """
+    Plot images with attribution map contours
+    """
+    figs = {}
+    vmax = np.percentile(np.absolute(images), 99)
+    for algorithm, heatmap in attribution_maps.items():
+        vmax_heatmap = np.percentile(np.absolute(heatmap), 99) # perceptive max: 0.0115
+        vmax_level = vmax_heatmap * 0.8
+        figs[algorithm] = []
+        for t in range(len(images)):
+            mask = heatmap[t][0,0]
+            mask_smoothed = gaussian_filter(mask, sigma=1)
+
+            fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10,5))
+            ax[0].imshow(images[0][0,0], vmin=-vmax, vmax=vmax, cmap='gray')
+            ax[0].contour(mask_smoothed, vmin=-vmax_level, vmax=vmax_level,
+                   levels=0.9 * np.array([-vmax_level, vmax_level]), # should be the most contrastive color so vmin and vmax
+                   cmap='RdBu_r')
+
+            ax[1].imshow(images[t][0,0], vmin=-vmax, vmax=vmax, cmap='gray')
+            ax[1].contour(mask_smoothed, vmin=-vmax_level, vmax=vmax_level,
+                   levels=0.9 * np.array([-vmax_level, vmax_level]), # should be the most contrastive color so vmin and vmax
+                   cmap='RdBu_r')
+            figs[algorithm].append(fig)
+    return figs
 
 
 def plot_heatmaps(imgs, zmin, zmax, color_continuous_scale, animation_frame=None, facet_col=None):
