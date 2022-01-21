93a1542a6955bafb670c3f9329d918a525a01aeb
Zeyu Sun <zeyusun@umich.edu>
2022-01-20 21:11:44 -0500

    leaderboard8:reproduce_3

--- run_arnet.py
+++ run_arnet.py
@@ -137,7 +137,7 @@ def sweep():
     parser.add_argument('-c', '--config_root', default='arnet/configs')
     parser.add_argument('-s', '--smoke', action='store_true')
     parser.add_argument('-e', '--experiment_name', default='leaderboard8')
-    parser.add_argument('-r', '--run_name', default='reproduce')
+    parser.add_argument('-r', '--run_name', default='reproduce_3')
     parser.add_argument('opts', default=None, nargs=argparse.REMAINDER)
     args = parser.parse_args()
     if args.smoke:
